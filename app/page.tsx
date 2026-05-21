7cd8f3cf3aa595297e451489da54daab18216549
humzamohammad <humzamohammad373@gmail.com>
2026-05-21 08:56:50 -0700

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white text-black">
+      <div className="text-3xl font-semibold">ghost AI</div>
+    </div>
+  );
+}
